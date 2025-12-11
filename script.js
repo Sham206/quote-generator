1b21317c66093e2b86f199b48e2032b88c50683c
Sham206 <shamsubramanian06@gmail.com>
2025-12-11 10:55:56 +0530

    Add loading state and safe quote handling

--- script.js
+++ script.js
@@ -5,10 +5,19 @@ const quotes = [
   "Pain today, pride tomorrow.",
   "Consistency wins."
 ];
-
-const res = document.getElementById('res');
-document.getElementById('btn').onclick=quotegenerator;
-function quotegenerator(){
-    const random_num=(Math.floor(Math.random()*quotes.length));
-    res.textContent=quotes[random_num];
-}
+const btn=document.getElementById('btn');
+const res=document.getElementById('res');
+btn.addEventListener('click',() =>{
+  res.textContent='Loading....';
+  btn.disabled=true;
+  setTimeout(() => {
+    if (quotes.length===0){
+      res.textContent="quotes are not available";
+    }
+    else{
+      const random_num=Math.floor((Math.random()*quotes.length));
+      res.textContent=quotes[random_num];
+    }
+    btn.disabled=false;
+  },1000);
+})
